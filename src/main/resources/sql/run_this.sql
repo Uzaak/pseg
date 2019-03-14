7f737c95fce1faf06f240320a312faf600db996c
Tiago Felisoni Furlanetto <tiago.f.furlanetto@gmail.com>
2019-03-14 00:12:19 -0300

    SQL fixes

--- src/main/resources/sql/run_this.sql
+++ src/main/resources/sql/run_this.sql
@@ -1,9 +1,9 @@
-create database tiago;
+create database pseg;
 
-use tiago;
+use pseg;
 
-create table credit_card (
-    id primary key auto_increment,
+create table `credit_card` (
+    id int primary key auto_increment,
     card_number bigint,
     holder_id bigint,
     holder varchar(255),
@@ -11,25 +11,25 @@ create table credit_card (
     expiration date
 );
 
-create table product (
-    id primary key auto_increment,
+create table `product` (
+    id int primary key auto_increment,
     description varchar(255),
     price decimal(10,2)
 );
 
-create table user (
-    id primary key auto_increment,
+create table `user` (
+    id int primary key auto_increment,
     name varchar(255),
     cpf bigint,
     email varchar(255),
     password varchar(255)
-)
+);
 
 create table transaction (
-    id primary key auto_increment,
+    id int primary key auto_increment,
     user_id bigint,
     credit_card_number bigint,
     credit_card_holder varchar(255),
     product_id bigint,
     paid bit
-)
+);
