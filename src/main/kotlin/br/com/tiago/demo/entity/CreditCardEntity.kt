dec6d5435f52172c982bb87d6c0d1f10cc4ed0fc
Tiago Felisoni Furlanetto <tiago.f.furlanetto@gmail.com>
2019-03-13 23:09:04 -0300

    Card Table and Entity mapping

--- src/main/kotlin/br/com/tiago/demo/entity/CreditCardEntity.kt
+++ src/main/kotlin/br/com/tiago/demo/entity/CreditCardEntity.kt
@@ -2,15 +2,25 @@ package br.com.tiago.demo.entity
 
 import br.com.tiago.demo.model.CreditCard
 import java.util.*
+import javax.persistence.*
 
+@Entity
+@Table(name = "credit_card")
 data class CreditCardEntity (
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long?,
 
+        @Column(name = "card_number")
         var number: Long,
+        @Column(name = "holder_id")
         var holderId: Long?,
+        @Column
         var holder: String,
+        @Column
         var cvv: Int?,
+        @Column
         var expiration: Date?
 
 ) {
--- src/main/resources/sql/run_this.sql
+++ src/main/resources/sql/run_this.sql
@@ -2,4 +2,12 @@ create database tiago;
 
 use tiago;
 
-create table user....
+create table credit_card (
+    id primary key auto_increment,
+    card_number bigint,
+    holder_id bigint,
+    holder varchar(255),
+    cvv int,
+    expiration date
+);
+
